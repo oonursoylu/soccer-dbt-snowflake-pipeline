850d40c671c3fa07825dd7c3fdf10cec49f45329
oonursoylu <oonursoylu@gmail.com>
2026-04-20 17:59:54 +0200

    refactor: drop records with missing league ids

--- models/intermediate/int_league_season_stats.sql
+++ models/intermediate/int_league_season_stats.sql
@@ -10,12 +10,11 @@ summary as (
     select
         l.league_name,
         m.season,
-        -- I am calculating high-level season metrics
         count(m.match_id) as total_matches,
         sum(m.home_team_goal + m.away_team_goal) as total_goals,
-        cast(avg(m.home_team_goal + m.away_team_goal) as decimal(5,2)) as avg_goals_per_match
+        cast(avg(m.home_team_goal + m.away_team_goal) as decimal(4,2)) as avg_goals_per_match
     from matches m
-    left join leagues l on m.league_id = l.league_id
+    inner join leagues l on m.league_id = l.league_id
     group by 1, 2
 )
 
